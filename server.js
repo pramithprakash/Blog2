a21d08d6aad76f0391d2f8750862970291875239
Pramith Prakash <pprakash2@sapient.com>
2015-06-24 14:15:47 +0530

    24Jul

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use('/node_modules/angular', express.static(__dirname + '/node_modules/angular'));
+app.use('/node_modules/angular-sanitize', express.static(__dirname + '/node_modules/angular-sanitize'));
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
